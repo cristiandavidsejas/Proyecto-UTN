0aa192452c85713fa789999bf248a8c01df59604
cristiandavidsejas <davidcris.sejas@gmail.com>
2022-08-26 04:53:17 -0300

    se agregaron mas cosas

--- Front/src/components/layout/Header.js
+++ Front/src/components/layout/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
     return (
             <header className="header">
               
-                    <img className="Banner" src="img/Banner.png" width="101%" id="banner"/>
+                    <img className="Banner" src="img/newBanner.png" width="101%" id="banner"/>
                
                
             </header>
